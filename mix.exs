6f12f30bf783969e87f0490860f5e7aac4341acb
Joe-noh <goflb.jh@gmail.com>
2015-07-18 07:27:23 +0900

    Mix.Compile.Nif just invokes make

--- mix.exs
+++ mix.exs
@@ -1,30 +1,11 @@
 defmodule Mix.Tasks.Compile.Nif do
   use Mix.Task
 
-  @shortdoc "compile C source"
-
-  @compiler "clang"
-  @erl_flag "-I#{:code.root_dir}/erts-#{:erlang.system_info :version}/include"
-  @c_files  [__DIR__, "c_src", "*.c"] |> Path.join |> Path.wildcard
-  @out_opt  "-o #{Path.join [__DIR__, "priv", "iconv.so"]}"
+  @shortdoc "invoke make"
 
   def run(_) do
-    [__DIR__, "priv"]
-    |> Path.join
-    |> File.mkdir_p!
-
-    [@compiler, @erl_flag, @c_files, shared_opts, @out_opt]
-    |> List.flatten
-    |> Enum.join(" ")
-    |> Mix.shell.cmd
-  end
-
-  defp shared_opts, do: ["-shared" | os_shared_opts]
-
-  defp os_shared_opts do
-    case :os.type do
-      {:unix, :darwin} -> ~w(-dynamiclib -undefined dynamic_lookup)
-      _other -> []
+    if Mix.shell.cmd("make") != 0 do
+      raise Mix.Error, "make failed"
     end
   end
 end
